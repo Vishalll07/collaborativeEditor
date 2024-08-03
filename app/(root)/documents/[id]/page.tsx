60f398d9315e2fd680710de9683bc046dd22488c
VISHAL SAHOO <bhagis052@gmail.com>
2024-08-04 00:14:57 +0530

    header changes for input text part

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/Header'
+import { Editor } from '@/components/ui/editor/Editor'
+
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <p>Test</p>
+      </Header>
+      <Editor />
+    </div>
+  )
+}
+
+export default Document
