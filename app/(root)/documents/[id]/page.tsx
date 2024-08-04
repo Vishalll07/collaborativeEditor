cde07c759de26c6099ca8f29b54aba2895e0f682
VISHAL SAHOO <bhagis052@gmail.com>
2024-08-04 23:53:09 +0530

    Clerk integrated

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,21 @@
 import Header from '@/components/Header'
 import { Editor } from '@/components/ui/editor/Editor'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 
 
 const Document = () => {
   return (
     <div>
       <Header>
-        <p>Test</p>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className='document title'>Share</p>
+        </div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
